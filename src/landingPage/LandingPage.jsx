fbb79982a419756c3ae5abeadff2c9614205aa91
Akshun-01 <akshunkuthiala2002@gmail.com>
2022-10-04 18:48:15 +0530

    improve timer ui

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -6,11 +6,11 @@ const LandingPage = () => {
 
     return (
         <div className="container">
-            < CountdownTimer />
-            
+
             <img src="../images/hillfair-logo-light.png" alt="hill'ffair logo" id='logo'/>
             <h1 className='coming-soon'>COMING SOON ...</h1>
 
+            < CountdownTimer />
         </div>
     );
 }
