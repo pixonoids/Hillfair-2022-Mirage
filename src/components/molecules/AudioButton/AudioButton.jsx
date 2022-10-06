0ee0f6702908efcb1e4a848e377520efed9c9822
Aditya Maurya <adityamaurya3435@gmail.com>
2022-10-06 19:24:36 +0530

    changed time and audio

--- src/components/molecules/AudioButton/AudioButton.jsx
+++ src/components/molecules/AudioButton/AudioButton.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { MdOutlineMusicNote, MdOutlineMusicOff } from 'react-icons/md';
 import './AudioButton.scss';
 const useAudio = () => {
-  const [audio] = useState(new Audio('/audio/Hillfair_audio_H.wav'));
+  const [audio] = useState(new Audio('/audio/theme-song.mp4'));
 
   const [playing, setPlaying] = useState(true);
 
