e71dba25c525c1adac65394402616e3d2277942e
Aditya Maurya <adityamaurya3435@gmail.com>
2022-10-06 22:19:48 +0530

    changed procfile

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 import './index.css'
 
+
+//
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
