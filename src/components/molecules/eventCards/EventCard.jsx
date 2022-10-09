5646f6cbf6cc2343385879b0d9b1964f96a97e40
Aditya Maurya <adityamaurya3435@gmail.com>
2022-10-09 15:24:13 +0530

    created basic layout

--- src/components/molecules/eventCards/EventCard.jsx
+++ src/components/molecules/eventCards/EventCard.jsx
@@ -1,9 +1,16 @@
 import './EventCard.scss';
 
-export default EventCard = ({data}) => {
+const EventCard = ({ data }) => {
     return (
         <div className='eventCard'>
-            <h1>Hi</h1>
+            <div className='eventCardItem'>
+                <h1>{data.event}</h1>
+                <h2>{data.club}</h2>
+                <h1>{data.date}</h1>
+                <p>{data.tag}</p>
+            </div>
+            <span className='circle'></span>
         </div>
     );
 }
+export default EventCard;
--- src/pages/eventPage/EventPage.jsx
+++ src/pages/eventPage/EventPage.jsx
@@ -1,13 +1,20 @@
 import './EventPage.scss';
 import Data from './data';
+import EventCard from '../../components/molecules/eventCards/EventCard';
 
 
 const Event = () => {
     return (
-        <div className='eventContainer'>
-            <h1>Hello</h1>
+        <div className='timelineContainer'>
+            <h1>Events</h1>
+            <div className='eventPage'>
+                {Data.map((data, id) => (
+                    <EventCard data={data} key={id} />
+                ))}
+            </div>
         </div>
     );
 }
 
 export default Event;
+
