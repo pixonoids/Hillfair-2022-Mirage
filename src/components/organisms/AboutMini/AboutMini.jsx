52c3cc632fabcd21298561921a381b6c97f84153
Yajat Chaudhary <chaudharyyajat@gmail.com>
2022-10-10 21:28:49 +0530

    home page basic

--- src/components/organisms/AboutMini/AboutMini.jsx
+++ src/components/organisms/AboutMini/AboutMini.jsx
@@ -5,7 +5,7 @@ export default function AboutMini() {
   return (
     <div className='aboutmini'>
         <div className="aboutmini-container">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 30" className='ele elei'><path  fill="#3A342E" fill-rule="evenodd" d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z" clip-rule="evenodd"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 30" className='ele elei'><path  fill="#956c4d" fill-rule="evenodd" d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z" clip-rule="evenodd"></path></svg>
         <div className="abouttext">
             <h1>About</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque perferendis officia, eius atque sunt odit facere? Ratione accusantium, dolorem doloribus ad deleniti omnis temporibus nisi laboriosam nobis perferendis, ipsa corrupti veniam sequi ea quisquam nulla quibusdam asperiores quaerat recusandae unde. Veritatis et labore possimus vitae illum sapiente quas voluptates numquam quisquam magni nihil, ipsum consequuntur delectus, quis est blanditiis. Ullam!
@@ -13,7 +13,7 @@ export default function AboutMini() {
             <button>View More <AiOutlineArrowRight className='icon'/></button>
         </div>
         
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 30" className='ele'><path  fill="#3A342E" fill-rule="evenodd" d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z" clip-rule="evenodd"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 30" className='ele'><path  fill="#956c4d" fill-rule="evenodd" d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z" clip-rule="evenodd"></path></svg>
         </div>
     </div>
   )
--- src/components/organisms/HomeMain/HomeMain.jsx
+++ src/components/organisms/HomeMain/HomeMain.jsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 import "./HomeMain.scss";
 const HomeMain = () => {
+  const [valueScn, setValueScn] = useState('0');
+  window.addEventListener('scroll', () => {
+    setValueScn(window.scrollY);
+  });
+  const [offset, setOffset] = useState('0');
   return (
     <div className="homeMain">
-      <div className="text">
+      <img
+        className="layers"
+        src="/public/images/sunlayer-01.png"
+        style={{ transform: `translateY(${-valueScn * 0.3}px)` }}
+        alt="hello"
+      ></img>
+      <img
+        className="layers"
+        src="/public/images/cloudslayer.png"
+        style={{ transform: `translateY(${-valueScn * 0.2}px)` }}
+        alt="hello"
+      ></img>
+      <img
+        className="layers"
+        src="/public/images/sandlayer-01.png"
+        style={{ transform: `translateY(${-valueScn * 0.1}px)` }}
+        alt="hello"
+      ></img>
+      <div className="header-text">
+        <div className="heading">
+          <h1>Hill'ffair 2k22</h1>
+          <h3>Mirage: Oasis of Mystique</h3>
+          {/* <Link key="/register" to="/register">
+            <HillffairButton btnText="register here"></HillffairButton>
+          </Link> */}
+        </div>
+        <div className="dates">
+          <h1>11-13</h1>
+          <h3>November 2022</h3>
+        </div>
+      </div>
+      {/* <div className="text">
         <span>Hill'ffair 2K22</span>
         <p className="theme">Mirage: Oasis of Mystique</p>
         <p className="dates">11 Nov - 13 Nov</p>
         <button>Register Now</button>
-      </div>
-      {/* <img
-        src="/public/images/hillffair home main.png"
-        className="mainImg"
-        alt=""
-      /> */}
+      </div> */}
     </div>
   );
 };
