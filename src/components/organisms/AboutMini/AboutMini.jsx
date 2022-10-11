57c1b27df1a5ec11e466f7a0e9452127940dfefb
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2022-10-11 13:33:02 +0530

    homepage responsive

--- src/components/organisms/AboutMini/AboutMini.jsx
+++ src/components/organisms/AboutMini/AboutMini.jsx
@@ -8,8 +8,7 @@ export default function AboutMini() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 95 30" className='ele elei'><path  fill="#956c4d" fill-rule="evenodd" d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z" clip-rule="evenodd"></path></svg>
         <div className="abouttext">
             <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque perferendis officia, eius atque sunt odit facere? Ratione accusantium, dolorem doloribus ad deleniti omnis temporibus nisi laboriosam nobis perferendis, ipsa corrupti veniam sequi ea quisquam nulla quibusdam asperiores quaerat recusandae unde. Veritatis et labore possimus vitae illum sapiente quas voluptates numquam quisquam magni nihil, ipsum consequuntur delectus, quis est blanditiis. Ullam!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque perferendis officia, eius atque sunt odit facere? Ratione accusantium, dolorem doloribus ad deleniti omnis temporibus nisi laboriosam nobis perferendis, ipsa corrupti veniam sequi ea quisquam nulla quibusdam asperiores quaerat recusandae unde. Veritatis et labore possimus vitae illum sapiente quas voluptates numquam quisquam magni nihil, ipsum consequuntur delectus, quis est blanditiis. Ullam!</p>
+            <p>India is rich in culture and diversity. To celebrate our homeland's heritage and culture, every year this fiesta of hilarity and hysterics, draws thousands of students to participate in this captivating festival. Hill'ffair offers the participants the chance to unleash their hidden talents from a plethora of genres ranging from creative skills to singing, dancing, acting, and finally rocking up the stage with band music and fashion show. </p>
             <button>View More <AiOutlineArrowRight className='icon'/></button>
         </div>
         
