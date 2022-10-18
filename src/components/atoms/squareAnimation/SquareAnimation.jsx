5129235b1573b62f1fd65fd9b16b327291b74968
Aditya Maurya <adityamaurya3435@gmail.com>
2022-10-19 04:06:55 +0530

    added funfact animation

---
+++ src/components/atoms/squareAnimation/SquareAnimation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SquareAnimation.scss'
+const SquareAnimation = () => {
+    return (
+        <div className="sk-folding-cube">
+            <div className="sk-cube1 sk-cube"></div>
+            <div className="sk-cube2 sk-cube"></div>
+            <div className="sk-cube4 sk-cube"></div>
+            <div className="sk-cube3 sk-cube"></div>
+        </div>
+    )
+}
+export default SquareAnimation;
