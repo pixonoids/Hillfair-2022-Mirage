2fe919e6dde6728502887c60a90ac6dffcc9c658
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-04 04:37:20 +0530

    added animations for event cards

--- src/pages/eventPage/data.jsx
+++ src/pages/eventPage/data.jsx
@@ -1,3 +1,7 @@
+import Brightness3Icon from '@mui/icons-material/Brightness3';
+import Brightness2Icon from '@mui/icons-material/Brightness2';
+import CircleIcon from '@mui/icons-material/Circle';
+
 const Data = [
     {
         event: "Halloween",
@@ -5,15 +9,17 @@ const Data = [
         date: "31st October",
         // tag: "Music",
         eventStatus: "Finished",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
+        moon: <Brightness3Icon/>
+        // moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
     },
     {
         event: "Bazzinga",
         club: "English Club",
         date: "3rd November",
         // tag: "Mystery",
-        eventStatus: "Ongoing",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
+        eventStatus: "Finished",
+        moon: <Brightness2Icon/>
+        // moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
     },
     {
         event: "Rhythm and Rhymes",
@@ -21,23 +27,27 @@ const Data = [
         date: "8th November",
         // tag: "Art",
         eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
+        moon: <CircleIcon/>
+
+        // moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
     },
-    {
-        event: "Orientation",
-        club: "First Year",
-        date: "10th- 11th November",
-        // tag: "Test",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
+    // {
+    //     event: "Orientation",
+    //     club: "First Year",
+    //     date: "10th- 11th November",
+    //     // tag: "Test",
+    //     eventStatus: "Coming",
+    //     moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
+    // },
     {
         event: "Campus Star",
         club: "In4mals",
         date: "13th November",
         // tag: "Music",
-        eventStatus: "Finished",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
+        eventStatus: "Coming",
+        moon: <Brightness2Icon/>
+
+        // moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
     },
     {
         event: "Open Mic",
@@ -45,7 +55,8 @@ const Data = [
         date: "15th November ",
         // tag: "Art",
         eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
+        moon: <Brightness3Icon/>
+        // moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="white" /></svg>
     }
 
 ]
