f41ae0cddf98032daeec28e4aad242fcd3b27348
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-04 02:50:03 +0530

    changed event elements

--- src/pages/eventPage/data.jsx
+++ src/pages/eventPage/data.jsx
@@ -1,132 +1,52 @@
 const Data = [
     {
-        event: "Music Night",
-        club: "Music Club",
-        date: "Nov 1, 2022",
-        tag: "Music",
+        event: "Halloween",
+        club: "Fashion Parade",
+        date: "31st October",
+        // tag: "Music",
         eventStatus: "Finished",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
     },
     {
-        event: "Mind Hunt",
-        club: "Hermatica",
-        date: "Nov 2, 2022",
-        tag: "Mystery",
+        event: "Bazzinga",
+        club: "English Club",
+        date: "3rd November",
+        // tag: "Mystery",
         eventStatus: "Ongoing",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
     },
     {
-        event: "Slice of Art",
-        club: "Fine arts",
-        date: "Nov 3, 2022",
-        tag: "Art",
+        event: "Rhythm and Rhymes",
+        club: "English Club",
+        date: "8th November",
+        // tag: "Art",
         eventStatus: "Coming",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
     },
     {
-        event: "Electro Blaze",
-        club: "Ojas",
-        date: "Nov 4, 2022",
-        tag: "Test",
+        event: "Orientation",
+        club: "First Year",
+        date: "10th- 11th November",
+        // tag: "Test",
         eventStatus: "Coming",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
     },
     {
-        event: "Music Night",
-        club: "Music Club",
-        date: "Nov 1, 2022",
-        tag: "Music",
+        event: "Campus Star",
+        club: "In4mals",
+        date: "13th November",
+        // tag: "Music",
         eventStatus: "Finished",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
     },
     {
-        event: "Mind Hunt",
-        club: "Hermatica",
-        date: "Nov 2, 2022",
-        tag: "Mystery",
-        eventStatus: "Ongoing",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Slice of Art",
-        club: "Fine arts",
-        date: "Nov 3, 2022",
-        tag: "Art",
+        event: "Open Mic",
+        club: "Team Public Relations",
+        date: "15th November ",
+        // tag: "Art",
         eventStatus: "Coming",
         moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Electro Blaze",
-        club: "Ojas",
-        date: "Nov 4, 2022",
-        tag: "Test",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Music Night",
-        club: "Music Club",
-        date: "Nov 1, 2022",
-        tag: "Music",
-        eventStatus: "Finished",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Mind Hunt",
-        club: "Hermatica",
-        date: "Nov 2, 2022",
-        tag: "Mystery",
-        eventStatus: "Ongoing",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Slice of Art",
-        club: "Fine arts",
-        date: "Nov 3, 2022",
-        tag: "Art",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Electro Blaze",
-        club: "Ojas",
-        date: "Nov 4, 2022",
-        tag: "Test",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Music Night",
-        club: "Music Club",
-        date: "Nov 1, 2022",
-        tag: "Music",
-        eventStatus: "Finished",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Mind Hunt",
-        club: "Hermatica",
-        date: "Nov 2, 2022",
-        tag: "Mystery",
-        eventStatus: "Ongoing",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Slice of Art",
-        club: "Fine arts",
-        date: "Nov 3, 2022",
-        tag: "Art",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
-    {
-        event: "Electro Blaze",
-        club: "Ojas",
-        date: "Nov 4, 2022",
-        tag: "Test",
-        eventStatus: "Coming",
-        moon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z" fill="rgba(241,196,14,1)" /></svg>
-    },
+    }
 
 ]
 export default Data;
