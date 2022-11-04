490f2668a721bb1b63a6df4829bc0d8114c5fcef
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-04 14:20:36 +0530

    improved model animation

--- src/pages/galleryPage/GalleryPage.jsx
+++ src/pages/galleryPage/GalleryPage.jsx
@@ -39,15 +39,15 @@ const GalleryPage = () => {
   return (
     <div className='gallery'>
       <div className="heading">OUR GALLERY</div>
+      <div className={Model ? "model open" : "model"}>
+          <img src={TempImgSrc} />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" onClick={() => setModel(false)}><path fill="none" d="M0 0h24v24H0z" /><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" fill='white' /></svg>
+        </div>
       <div className="row" onMouseMove={handleMouseMove}
       style={{
         translate: `-${globalCoords.x / 50 - 10}px -${globalCoords.y / 50}px`,
         transition:"translate 0.1s"
       }}>
-        <div className={Model ? "model open" : "model"}>
-          <img src={TempImgSrc} />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" onClick={() => setModel(false)}><path fill="none" d="M0 0h24v24H0z" /><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" fill='white' /></svg>
-        </div>
         <div className="column">
           {galleryData.map((data, id) => {
             return (<img src={data.src} style={{ width: "100%" }} onClick={() => getImg(data.src)} />)
