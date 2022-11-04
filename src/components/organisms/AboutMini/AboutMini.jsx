7928d6c902d0cbc8a89f40cc2418586780f94c03
Yajat Chaudhary <chaudharyyajat@gmail.com>
2022-11-04 08:18:21 +0100

    time changes in about mini

--- src/components/organisms/AboutMini/AboutMini.jsx
+++ src/components/organisms/AboutMini/AboutMini.jsx
@@ -33,7 +33,7 @@ export default function AboutMini() {
           className="ele elei"
         >
           <path
-            fill={hour >= 9 && hour <= 16 ? "#956c4d" : "#723B99"}
+            fill={hour >= 6 && hour <= 16 ? "#956c4d" : "#723B99"}
             fillRule="evenodd"
             d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z"
             clipRule="evenodd"
@@ -42,7 +42,7 @@ export default function AboutMini() {
         <div
           className="abouttext"
           style={{
-            backgroundColor: hour >= 9 && hour <= 16 ? "#956c4d" : "#723B99",
+            backgroundColor: hour >= 6 && hour <= 16 ? "#956c4d" : "#723B99",
           }}
         >
           <h1>About</h1>
@@ -76,7 +76,7 @@ export default function AboutMini() {
           className="ele"
         >
           <path
-            fill={hour >= 9 && hour <= 16 ? "#956c4d" : "#723B99"}
+            fill={hour >= 6 && hour <= 16 ? "#956c4d" : "#723B99"}
             fillRule="evenodd"
             d="M.229 0c19.113 2.098 36.903 8.842 46.99 30C57.515 8.842 75.249 2.099 94.29 0H.23Z"
             clipRule="evenodd"
