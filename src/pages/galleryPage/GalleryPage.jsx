3aedf34e9d23bb79cb7dff9431fb4cb549ce9921
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-04 16:58:30 +0530

    added close for whole model

--- src/pages/galleryPage/GalleryPage.jsx
+++ src/pages/galleryPage/GalleryPage.jsx
@@ -39,9 +39,9 @@ const GalleryPage = () => {
   return (
     <div className='gallery'>
       <div className="heading">OUR GALLERY</div>
-      <div className={Model ? "model open" : "model"}>
-          <img src={TempImgSrc} />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" onClick={() => setModel(false)}><path fill="none" d="M0 0h24v24H0z" /><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" fill='white' /></svg>
+      <div className={Model ? "model open" : "model"} onClick={() => setModel(false)}>
+          <img src={TempImgSrc}/>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" fill='white' /></svg>
         </div>
       <div className="row" onMouseMove={handleMouseMove}
       style={{
