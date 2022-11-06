7347edb553517513faeca266696a811f6146ba25
rehanmm <21bcs049@nith.ac.in>
2022-11-06 19:25:54 +0530

    added The Verhoeff algorithm for aadhaar validation

--- src/services/validation/guestValidation.js
+++ src/services/validation/guestValidation.js
@@ -1,12 +1,12 @@
 import * as yup from 'yup';
-import aadhaarValidate from '../../utils/aadhaarValidationAlgorithm';
 
+import checkUID from '../../utils/aadhaarValidationAlgorithm'
 yup.addMethod(yup.string, "aadhaarValidate", function (errorMessage) {
     return this.test(`aadhaar`, errorMessage, function (value) {
       const { path, createError } = this;
 
       return (
-       aadhaarValidate(value)||
+        checkUID(value)||
         createError({ path, message: errorMessage })
       );
     });
@@ -14,7 +14,7 @@ yup.addMethod(yup.string, "aadhaarValidate", function (errorMessage) {
 const guestValidation = yup.object().shape({
     designation: yup.string().required('Required'),
     social: yup.string().required('Required'),
-    aadhaar: yup.string().aadhaarValidate('aadhar is not valid').required('Required')
+    aadhaar: yup.string().aadhaarValidate('Aadhaar is not valid').required('Required')
 });
 
 export default guestValidation;
