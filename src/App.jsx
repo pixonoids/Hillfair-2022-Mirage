ad23f340ac271e6861ffdaec25df99714e0a8d2f
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-10 16:58:20 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ export default function App() {
             }
           />
           <Route path="sponsors" element={<LandingPage />} />
-          <Route path="team" element={<Team />} />
+          <Route path="team" element={<LandingPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="Footer" element={<LandingPage />} />
           <Route path="admin" element={<LandingPage />} />
