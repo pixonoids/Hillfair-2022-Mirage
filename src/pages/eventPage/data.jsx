aa952c5959891b398b03eb4baf74e1ae3978a98f
Aditya Maurya <adityamaurya3435@gmail.com>
2022-11-14 22:25:09 +0530

    removed events cards

--- src/pages/eventPage/data.jsx
+++ src/pages/eventPage/data.jsx
@@ -22,13 +22,13 @@ const Data = [
         image: '/images/eventPoster/bazzinga.jpeg',
     },
     {
-        event: "Rhythm and Rhymes",
+        event: "Synphonies of Sunsets",
         club: "English Club",
         date: "9th November",
         eventStatus: "Finished",
         moon: <Brightness3Icon />,
         time: "8:00AM-10:00PM",
-        // image: '/images/eventPoster/halloween.jpeg',
+        image: '/images/eventPoster/rhythm.jpeg',
     },
     {
         event: "Campus Star",
@@ -40,319 +40,319 @@ const Data = [
         image: '/images/eventPoster/campusStar.jpeg',
     },
     {
-        event: "Open Mic",
+        event: "Tryst With Autumn",
         club: "Team Public Relations",
         date: "15th November ",
         eventStatus: "Coming",
         moon: <CircleIcon />,
         time: "8:00AM-10:00PM",
-        // image: '/images/eventPoster/halloween.jpeg',
+        image: '/images/eventPoster/openMic.jpg',
     },
-    {
-        event: "Treasure Hunt",
-        club: "In4mals",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "10:00AM-12:00PM"
-    },
-    {
-        event: "Battle of Bands",
-        club: "Music Club",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "1:00PM-4:00PM"
-    },  
-    {
-        event: "Opening Ceremony",
-        club: "",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "4:30PM-5:30PM"
-    },   
+    // {
+    //     event: "Treasure Hunt",
+    //     club: "In4mals",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "10:00AM-12:00PM"
+    // },
+    // {
+    //     event: "Battle of Bands",
+    //     club: "Music Club",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "1:00PM-4:00PM"
+    // },  
+    // {
+    //     event: "Opening Ceremony",
+    //     club: "",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "4:30PM-5:30PM"
+    // },   
     
-    {
-        event: "Devotional Dance",
-        club: "Rhythmeecz",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "4:30PM-5:30PM"
-    },   
-    {
-        event: "Dravida Dance",
-        club: "Dravida",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "4:30PM-5:30PM"   
-    },   
-    {
-        event: "Dramatics Performance 1",
-        club: "Dramatics",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:40PM-5:55PM"
-    },   
-    {
-        event: "Literacy Mission",
-        club: "",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:00PM-6:00PM"
-    },   
-    {
-        event: "Yogic Self",
-        club: "Yogic Self",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:13PM-6:20PM"
-    },   
     // {
-    //     event: "Band - HP14",
+    //     event: "Devotional Dance",
+    //     club: "Rhythmeecz",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "4:30PM-5:30PM"
+    // },   
+    // {
+    //     event: "Dravida Dance",
+    //     club: "Dravida",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "4:30PM-5:30PM"   
+    // },   
+    // {
+    //     event: "Dramatics Performance 1",
+    //     club: "Dramatics",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:40PM-5:55PM"
+    // },   
+    // {
+    //     event: "Literacy Mission",
     //     club: "",
     //     date: "18th November",
     //     eventStatus: "Coming",
     //     moon: <Brightness3Icon />,
-    //     time: "6:45PM-7:45PM"
+    //     time: "6:00PM-6:00PM"
     // },   
-    {
-        event: "Bhangra Performance 1",
-        club: "Bhangra Club",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "7:55PM-8:07PM"
-    },   
-    {
-        event: "Fashion Show",
-        club: "Fash P",
-        date: "18th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "8:12PM-8:22PM"
-    },   
     // {
-    //     event: "Artist (DJ Tejas)",
-    //     club: "",
+    //     event: "Yogic Self",
+    //     club: "Yogic Self",
     //     date: "18th November",
     //     eventStatus: "Coming",
     //     moon: <Brightness3Icon />,
-    //     time: "8:30PM-10:00PM"
+    //     time: "6:13PM-6:20PM"
     // },   
-    {
-        event: "Fashion Show",
-        club: "FashP",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "12:00PM-12:30PM"
-    },   
-    {
-        event: "Music Competition",
-        club: "Music Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "1:30PM-2:30PM"
-    },   
-    {
-        event: "Dramatics Play (Auditorium)",
-        club: "Dramatics Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "2:30PM-3:30PM"
-    },   
-    {
-        event: "Dravida Classical",
-        club: "Dravida Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:00AM-5:07PM"
-    },   
-    {
-        event: "NSS",
-        club: "NSS",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:08AM-5:15PM"
-    },   
-    {
-        event: "Karate",
-        club: "Karate",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:18PM-5:25PM"
-    },   
-    {
-        event: "Dance",
-        club: "Rhythmeecz",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:28PM-5:35PM"
-    },   
-    {
-        event: "Yogic Self",
-        club: "Yogic Self",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:38PM-5:45PM"
-    },   
-    {
-        event: "Nati Club Performance",
-        club: "Nati Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:50PM-6:10PM"
-    },   
-    {
-        event: "FashP Performance 1",
-        club: "FashP",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:15PM-6:25PM"
-    },   
-    {
-        event: "MIME",
-        club: "Dramatics",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:25PM-6:40PM"
-    },   
-    {
-        event: "English Club Skit",
-        club: "English Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:45PM-7:00PM"
-    },   
-    {
-        event: "Bhangra Performance 2",
-        club: "Bhangra Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "7:05PM-7:17PM"
-    },   
-    {
-        event: "Music Club 1",
-        club: "Music Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "7:20PM-8:20PM"
-    },   
-    {
-        event: "Dramatics Skit",
-        club: "Dramatics Club",
-        date: "19th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "8:25PM-8:40PM"
-    },   
+    // // {
+    // //     event: "Band - HP14",
+    // //     club: "",
+    // //     date: "18th November",
+    // //     eventStatus: "Coming",
+    // //     moon: <Brightness3Icon />,
+    // //     time: "6:45PM-7:45PM"
+    // // },   
     // {
-    //     event: "Comedian",
-    //     club: "--",
+    //     event: "Bhangra Performance 1",
+    //     club: "Bhangra Club",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "7:55PM-8:07PM"
+    // },   
+    // {
+    //     event: "Fashion Show",
+    //     club: "Fash P",
+    //     date: "18th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "8:12PM-8:22PM"
+    // },   
+    // // {
+    // //     event: "Artist (DJ Tejas)",
+    // //     club: "",
+    // //     date: "18th November",
+    // //     eventStatus: "Coming",
+    // //     moon: <Brightness3Icon />,
+    // //     time: "8:30PM-10:00PM"
+    // // },   
+    // {
+    //     event: "Fashion Show",
+    //     club: "FashP",
     //     date: "19th November",
     //     eventStatus: "Coming",
     //     moon: <Brightness3Icon />,
-    //     time: "8:45M-9:45PM"
+    //     time: "12:00PM-12:30PM"
     // },   
-    {
-        event: "Dance Competition",
-        club: "Rhythmeecz",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "12:00PM-1:00PM"
-    },   
-    {
-        event: "Dramatics Play",
-        club: "Dramatics",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "2:00PM-3:00PM"
-    },    
-    {
-        event: "Devotional Dance",
-        club: "Rhythmeecz",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:00PM-5:08PM"
-    },   
-    {
-        event: "Music Club Performance",
-        club: "Music Club",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "5:15PM-6:10PM"
-    },     
-    {
-        event: "FashP Performance 2",
-        club: "FashP",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:15PM-6:30PM"
-    },     
-    {
-        event: "Dravida",
-        club: "Dravida",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "12:00PM-1:00PM"
-    },     
-    {
-        event: "Closing Ceremony",
-        club: "--",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "6:35PM-7:35PM"
-    },    
-    {
-        event: "Nati Club Performance",
-        club: "Nati Club",
-        date: "20th November",
-        eventStatus: "Coming",
-        moon: <Brightness3Icon />,
-        time: "7:50PM-8:00PM"
-    },    
     // {
-    //     event: "TRAP Band",
-    //     club: "STAR NIGHT",
+    //     event: "Music Competition",
+    //     club: "Music Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "1:30PM-2:30PM"
+    // },   
+    // {
+    //     event: "Dramatics Play (Auditorium)",
+    //     club: "Dramatics Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "2:30PM-3:30PM"
+    // },   
+    // {
+    //     event: "Dravida Classical",
+    //     club: "Dravida Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:00AM-5:07PM"
+    // },   
+    // {
+    //     event: "NSS",
+    //     club: "NSS",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:08AM-5:15PM"
+    // },   
+    // {
+    //     event: "Karate",
+    //     club: "Karate",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:18PM-5:25PM"
+    // },   
+    // {
+    //     event: "Dance",
+    //     club: "Rhythmeecz",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:28PM-5:35PM"
+    // },   
+    // {
+    //     event: "Yogic Self",
+    //     club: "Yogic Self",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:38PM-5:45PM"
+    // },   
+    // {
+    //     event: "Nati Club Performance",
+    //     club: "Nati Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:50PM-6:10PM"
+    // },   
+    // {
+    //     event: "FashP Performance 1",
+    //     club: "FashP",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "6:15PM-6:25PM"
+    // },   
+    // {
+    //     event: "MIME",
+    //     club: "Dramatics",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "6:25PM-6:40PM"
+    // },   
+    // {
+    //     event: "English Club Skit",
+    //     club: "English Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "6:45PM-7:00PM"
+    // },   
+    // {
+    //     event: "Bhangra Performance 2",
+    //     club: "Bhangra Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "7:05PM-7:17PM"
+    // },   
+    // {
+    //     event: "Music Club 1",
+    //     club: "Music Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "7:20PM-8:20PM"
+    // },   
+    // {
+    //     event: "Dramatics Skit",
+    //     club: "Dramatics Club",
+    //     date: "19th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "8:25PM-8:40PM"
+    // },   
+    // // {
+    // //     event: "Comedian",
+    // //     club: "--",
+    // //     date: "19th November",
+    // //     eventStatus: "Coming",
+    // //     moon: <Brightness3Icon />,
+    // //     time: "8:45M-9:45PM"
+    // // },   
+    // {
+    //     event: "Dance Competition",
+    //     club: "Rhythmeecz",
     //     date: "20th November",
     //     eventStatus: "Coming",
     //     moon: <Brightness3Icon />,
-    //     time: "8:05PM-9:05PM"
+    //     time: "12:00PM-1:00PM"
     // },   
     // {
-    //     event: "DJ Zephytrone",
-    //     club: "STAR NIGHT",
+    //     event: "Dramatics Play",
+    //     club: "Dramatics",
     //     date: "20th November",
     //     eventStatus: "Coming",
     //     moon: <Brightness3Icon />,
-    //     time: "9:05PM-10:00PM"
-    // },
+    //     time: "2:00PM-3:00PM"
+    // },    
+    // {
+    //     event: "Devotional Dance",
+    //     club: "Rhythmeecz",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:00PM-5:08PM"
+    // },   
+    // {
+    //     event: "Music Club Performance",
+    //     club: "Music Club",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "5:15PM-6:10PM"
+    // },     
+    // {
+    //     event: "FashP Performance 2",
+    //     club: "FashP",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "6:15PM-6:30PM"
+    // },     
+    // {
+    //     event: "Dravida",
+    //     club: "Dravida",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "12:00PM-1:00PM"
+    // },     
+    // {
+    //     event: "Closing Ceremony",
+    //     club: "--",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "6:35PM-7:35PM"
+    // },    
+    // {
+    //     event: "Nati Club Performance",
+    //     club: "Nati Club",
+    //     date: "20th November",
+    //     eventStatus: "Coming",
+    //     moon: <Brightness3Icon />,
+    //     time: "7:50PM-8:00PM"
+    // },    
+    // // {
+    // //     event: "TRAP Band",
+    // //     club: "STAR NIGHT",
+    // //     date: "20th November",
+    // //     eventStatus: "Coming",
+    // //     moon: <Brightness3Icon />,
+    // //     time: "8:05PM-9:05PM"
+    // // },   
+    // // {
+    // //     event: "DJ Zephytrone",
+    // //     club: "STAR NIGHT",
+    // //     date: "20th November",
+    // //     eventStatus: "Coming",
+    // //     moon: <Brightness3Icon />,
+    // //     time: "9:05PM-10:00PM"
+    // // },
 
 ]
 export default Data;
